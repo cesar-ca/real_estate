e01e737e7f71f21b43a8cd619b5a89bef55e7a47
Emanuel Castro <emanuel@minerva.kgi.edu>
2021-04-10 19:52:15 -0700

    🗃 creating database models

--- real_estate.py
+++ real_estate.py
@@ -1,31 +1,25 @@
+from datetime import datetime
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
+
 app = Flask(__name__)
-
-'''
-Inserting data
-
-Whenever a house is listed then the following things need to happen:
-
-All the relevant details of that house need to be captured, ie. 
-at least: seller details, # of bedrooms, # of bathrooms, listing price, 
-zip code, date of listing, the listing estate agent, 
-and the appropriate office.
-Whenever a house is sold then the following things need to happen:
-
-The estate agent commission needs to be calculated. This happens on a sliding scale:
-For houses sold below $100,000 the commission is 10%
-For houses between $100,000 and $200,000 the commission is 7.5%
-For houses between $200,000 and $500,000 the commission is 6%
-For houses between $500,000 and $1,000,000 the commission is 5%
-For houses above $1,000,000 the commission is 4%
-A summary table containing the sum of all sale prices must be updated.
-All appropriate details related to the sale must be captured, ie. at least: 
-buyer details, sale price, date of sale, the selling estate agent.
-The original listing must be marked as sold.
-'''
-
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+db = SQLAlchemy(app)
+
+# The Listing class holds the real estate listings
+class Listing(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    seller = db.Column(db.String(100), nullable=False)
+    date = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    bedrooms = db.Column(db.String(100), nullable=False)
+    bathrooms = db.Column(db.String(100), nullable=False)
+    zipcode = db.Column(db.String(100), nullable=False)
+    agent = db.Column(db.String(100), nullable=False)
+    office = db.Column(db.String(100), nullable=False)
+
+    def __repr__(self):
+        return f"Listing('{self.seller}', '{self.date}')"
+
 
 listings = [
     {
