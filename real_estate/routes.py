7aa593ac0a0d0c121b7b421d94df61a6772d343c
Emanuel Castro <emanuel@minerva.kgi.edu>
2021-04-11 00:22:08 -0700

    🗃 succesful insertion of data!

--- real_estate/routes.py
+++ real_estate/routes.py
@@ -18,8 +18,8 @@ def about():
 def new_listing():
     form = ListingForm()
     if form.validate_on_submit():
-        listing = Listing(seller=form.seller.data, bedrooms=form.bedrooms.data, bathrooms=form.bathrooms.data, zipcode=form.zipcode.data, office=form.office.data, agent_id=form.agent_id.data)
-        #listing = Listing(seller=form.seller.data, price=form.price.data, bedrooms=form.bedrooms.data, bathrooms=form.bathrooms.data, zipcode=form.zipcode.data, office=form.office.data, agent_id=form.agent_id.data)
+        #listing = Listing(seller=form.seller.data, bedrooms=form.bedrooms.data, bathrooms=form.bathrooms.data, zipcode=form.zipcode.data, office=form.office.data, agent_id=form.agent_id.data)
+        listing = Listing(seller=form.seller.data, price=form.price.data, bedrooms=form.bedrooms.data, bathrooms=form.bathrooms.data, zipcode=form.zipcode.data, office=form.office.data, agent_id=form.agent_id.data)
         db.session.add(listing)
         db.session.commit()
         flash('The listing has been created!', 'success')
